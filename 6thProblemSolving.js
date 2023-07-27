2c872e11b7ae1056158baf8f6246f44994e946c6
najmul905 <nh4808563@gmail.com>
2023-07-27 19:15:50 +0600

    solved 6th problem

---
+++ 6thProblemSolving.js
@@ -0,0 +1,17 @@
+const createRandomPassword=(length)=> {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+    let password = "";
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+  
+    return password;
+  }
+  
+ 
+  const passwordLength = 12; 
+  const randomPassword = createRandomPassword(passwordLength);
+  console.log(randomPassword);
+  
